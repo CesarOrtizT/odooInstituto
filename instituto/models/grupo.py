1429930b9918583c7a6154abb4526c3a1e6ca546
isaacsp-0 <isaacsanchezpacheco19@hotmail.com>
2025-01-31 17:33:40 +0100

    Agrado método de restricción al campo name del modelo Grupo

--- instituto/models/grupo.py
+++ instituto/models/grupo.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class grupo(models.Model):
     _name = 'instituto.grupo'
@@ -16,9 +17,8 @@ class grupo(models.Model):
     curso = fields.Many2one("instituto.curso", string="Curso")
 
 
-
     @api.constrains('name')
     def comprobarDato(self):
         for record in self:
-            if (record.name.Siz):
-
+            if (record.name and len(record.name) > 1 ):
+                    raise ValidationError("¡El grupo no puede contener más de una letra como identificador!")
