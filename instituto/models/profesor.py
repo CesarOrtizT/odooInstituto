baf7c7777b6da64bcef2c922c1dc026e96d7d5a8
isaacsp-0 <isaacsanchezpacheco19@hotmail.com>
2025-02-05 20:37:36 +0100

    Agregado un comentario random

--- instituto/models/profesor.py
+++ instituto/models/profesor.py
@@ -24,6 +24,7 @@ class profesor(models.Model):
       
     especialidad = fields.Char(string = "Especialidad", required = True)
 
+# Añadido un comentario random
     @api.depends('fotoProfesor')
     def _compute_foto(self):
         for record in self:
