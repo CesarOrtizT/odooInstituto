03634d5d8336888c7a8fe749fbcd96c1728dde69
isaacsp-0 <isaacsanchezpacheco19@hotmail.com>
2025-02-18 16:08:09 +0100

    Agragada nueva restricción en el modelo curso

--- instituto/models/curso.py
+++ instituto/models/curso.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class curso(models.Model):
     _name = 'instituto.curso'
@@ -7,3 +8,10 @@ class curso(models.Model):
     name = fields.Integer(string="Número del curso", required = True)
 
     grupos = fields.Many2many("instituto.grupo", string="Grupos")
+     
+    
+    @api.constrains('name')
+    def _check_name(self):
+        for record in self:
+            if(record.name != record.grupos.curso):
+                raise ValidationError("No puede ser el número del curso distinto al del grupo.")
